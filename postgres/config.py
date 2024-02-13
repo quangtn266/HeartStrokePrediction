61b9d9ae15166f41ec5ccbaeda204e059a4c8abf
quangtn266 <quangtrandn93@gmail.com>
2024-02-13 10:29:48 +0700

    Update source code for postgresSQL section

--- postgres/config.py
+++ postgres/config.py
@@ -1,7 +1,7 @@
 import os
 from pathlib import Path
 from dotenv import load_dotenv
-load_dotenv(dotenv_path="../.env")
+load_dotenv(dotenv_path="/Users/quangtn/Desktop/01_work/01_job/02_ml/PySpark/HeartStrokePrediction/env")
 
 def get_setting():
     POSTGRES_USER : str = os.getenv("POSTGRES_USER")
@@ -12,3 +12,6 @@ def get_setting():
     DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
     return DATABASE_URL
+
+if __name__ == "__main__":
+    get_setting()
--- postgres/createdb.py
+++ postgres/createdb.py
@@ -5,7 +5,7 @@ print("Creating database")
 
 Base.metadata.create_all(engine)
 
-file_name = "../data/Training_data/healthcare-dataset-stroke-data.csv"
+file_name = "/Users/quangtn/Desktop/01_work/01_job/02_ml/PySpark/HeartStrokePrediction/data/healthcare-dataset-stroke-data.csv"
 df = pd.read_csv(file_name)
 df['time'] = datetime.now()
 df.to_sql(con=engine, index=False, name='data_trained', if_exists='replace')
